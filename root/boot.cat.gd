5ed24cde952f888fa1d5edeb84b52d9264365ffd
CatMeowByte <CatMeowByte@gmail.com>
2024-12-12 23:33:51 +0900

    Update boot.cat.gd

--- root/boot.cat.gd
+++ root/boot.cat.gd
@@ -1,20 +1,41 @@
 extends CAT32
 
-func beep(c, t, d):
-	DIS.clear(c)
-	DIS.flip()
-	SND.play_tone(t, d)
-	await timer(d)
+const APP = "/app/planet_name.cat.gd"
+
+const DELAY = 0.03125
+const FREQ_STEP = 1.12246
+const COLOR_COUNT = 16
+
+var current_freq = 440.0
+var stripe_height = DIS.H / COLOR_COUNT
 
 func init():
-	await beep(COL.BLACK, 0, 0.5)
-	await beep(COL.DARK_GRAY, 320.0, 0.125)
-	await beep(COL.BLACK, 0, 0.0625)
-	await beep(COL.DARK_GRAY, 320.0, 0.125)
-	await beep(COL.BLACK, 0, 0.25)
-	await beep(COL.WHITE, 880.0, 0.5)
-	await beep(COL.BLACK, 0, 0.25)
+	DIS.clear()
+	for i in range(COLOR_COUNT):
+		var y = i * stripe_height
+		DIS.rect(0, y, DIS.W, stripe_height, i, true)
+		DIS.flip()
+		SND.play_tone(current_freq, DELAY, true)
+		current_freq *= FREQ_STEP
+		await timer(DELAY)
 
-	run("/app/planet_name.cat.gd")
-	#run("/bin/fm.cat.gd")
-	#run("/bin/hi.cat.gd", {"uname"= "Cat"})
+	await timer(DELAY * 8)
+	DIS.clear()
+	DIS.flip()
+	SND.play_tone(SND.get_freq("C", 4), DELAY)
+	await timer(DELAY * 8)
+	DIS.text(0, 0 , "███ ███ ███     ███ ███", COL.WHITE)
+	DIS.text(0, 8 , "█   █ █  █        █   █", COL.WHITE)
+	DIS.text(0, 16, "█   ███  █  ███  ██   █", COL.WHITE)
+	DIS.text(0, 24, "█   █ █  █        █  █ ", COL.WHITE)
+	DIS.text(0, 32, "███ █ █  █      ███ ███", COL.WHITE)
+	DIS.text(0, 40, "CAT-32-GD", COL.WHITE)
+	DIS.flip()
+	SND.play_tone(SND.get_freq("D", 5), DELAY)
+	await timer(DELAY * 2)
+	DIS.text(0, DIS.H - 8, "0.0.0 Experimental", COL.DARK_GRAY)
+	DIS.flip()
+	SND.play_tone(SND.get_freq("G", 5), DELAY)
+	await timer(DELAY * 16)
+
+	run(APP)
